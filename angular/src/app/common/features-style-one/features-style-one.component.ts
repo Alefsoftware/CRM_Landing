a88d34e7dbbfb5c0a995a1becdc4aa082f9240f1
minawagdy <minaw77@gmail.com>
2025-12-03 13:52:59 +0200

    update features-style-one.component.ts

--- angular/src/app/common/features-style-one/features-style-one.component.ts
+++ angular/src/app/common/features-style-one/features-style-one.component.ts
@@ -3,8 +3,9 @@ import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-features-style-one',
-    imports: [RouterLink],
     templateUrl: './features-style-one.component.html',
-    styleUrls: ['./features-style-one.component.scss']
+    styleUrls: ['./features-style-one.component.scss'],
+    standalone: true,
+    imports: [RouterLink], // <-- this is OK if you use it in template
 })
-export class FeaturesStyleOneComponent {}
+export class FeaturesStyleOneComponent { }
