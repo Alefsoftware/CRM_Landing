efd687fc10e3a5c804eae3eea7b4c2b71f50d561
minawagdy <minaw77@gmail.com>
2025-12-22 15:48:11 +0200

    update privacy-policy-page.component.html and privacy-policy-page.component.ts

--- angular/src/app/pages/privacy-policy-page/privacy-policy-page.component.ts
+++ angular/src/app/pages/privacy-policy-page/privacy-policy-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
 import { RouterLink } from '@angular/router';
 import { NavbarStyleTwoComponent } from '../../common/navbar-style-two/navbar-style-two.component';
 import { DownloadAppStyleTwoComponent } from '../../common/download-app-style-two/download-app-style-two.component';
@@ -7,8 +11,45 @@ import { BackToTopComponent } from '../../common/back-to-top/back-to-top.compone
 
 @Component({
     selector: 'app-privacy-policy-page',
-    imports: [RouterLink, NavbarStyleTwoComponent, DownloadAppStyleTwoComponent, FooterStyleFourComponent, BackToTopComponent],
+    standalone: true,
+    imports: [
+        CommonModule,
+        HttpClientModule,
+        RouterLink,
+        NavbarStyleTwoComponent,
+        DownloadAppStyleTwoComponent,
+        FooterStyleFourComponent,
+        BackToTopComponent
+    ],
     templateUrl: './privacy-policy-page.component.html',
     styleUrls: ['./privacy-policy-page.component.scss']
 })
-export class PrivacyPolicyPageComponent { }
+export class PrivacyPolicyPageComponent implements OnInit {
+
+    title = '';
+    content!: SafeHtml;
+
+    constructor(
+        private http: HttpClient,
+        private sanitizer: DomSanitizer
+    ) { }
+
+    ngOnInit(): void {
+        this.getPrivacyPolicy();
+    }
+
+    getPrivacyPolicy(): void {
+        this.http
+            .get<any>('https://admin.realstatecrm-development.dev.alefsoftware.com/site/privacy-policy')
+            .subscribe(res => {
+                if (res?.status && res?.data) {
+                    this.title = res.data.title_en;
+
+                    // VERY IMPORTANT
+                    this.content = this.sanitizer.bypassSecurityTrustHtml(
+                        res.data.description_en
+                    );
+                }
+            });
+    }
+}
