a380b661a55c8006b11a1ba8741d3dc384cf0db3
minawagdy <minaw77@gmail.com>
2025-12-22 18:37:52 +0200

    update terms-conditions-page.component.html

--- angular/src/app/pages/terms-conditions-page/terms-conditions-page.component.ts
+++ angular/src/app/pages/terms-conditions-page/terms-conditions-page.component.ts
@@ -4,10 +4,11 @@ import { NavbarStyleThreeComponent } from '../../common/navbar-style-three/navba
 import { DownloadAppStyleTwoComponent } from '../../common/download-app-style-two/download-app-style-two.component';
 import { FooterStyleFourComponent } from '../../common/footer-style-four/footer-style-four.component';
 import { BackToTopComponent } from '../../common/back-to-top/back-to-top.component';
+import { NavbarStyleTwoComponent } from "../../common/navbar-style-two/navbar-style-two.component";
 
 @Component({
     selector: 'app-terms-conditions-page',
-    imports: [RouterLink, NavbarStyleThreeComponent, DownloadAppStyleTwoComponent, FooterStyleFourComponent, BackToTopComponent],
+    imports: [RouterLink, NavbarStyleThreeComponent, DownloadAppStyleTwoComponent, FooterStyleFourComponent, BackToTopComponent, NavbarStyleTwoComponent],
     templateUrl: './terms-conditions-page.component.html',
     styleUrls: ['./terms-conditions-page.component.scss']
 })
