d8b89cd80d3874167a5112c42dd7c62457815cb6
minawagdy <minaw77@gmail.com>
2025-12-22 18:39:40 +0200

    update terms-conditions-page.component.ts

--- angular/src/app/pages/terms-conditions-page/terms-conditions-page.component.ts
+++ angular/src/app/pages/terms-conditions-page/terms-conditions-page.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { NavbarStyleThreeComponent } from '../../common/navbar-style-three/navbar-style-three.component';
+import { NavbarStyleTwoComponent } from '../../common/navbar-style-two/navbar-style-two.component';
 import { DownloadAppStyleTwoComponent } from '../../common/download-app-style-two/download-app-style-two.component';
 import { FooterStyleFourComponent } from '../../common/footer-style-four/footer-style-four.component';
 import { BackToTopComponent } from '../../common/back-to-top/back-to-top.component';
-import { NavbarStyleTwoComponent } from "../../common/navbar-style-two/navbar-style-two.component";
 
 @Component({
     selector: 'app-terms-conditions-page',
-    imports: [RouterLink, NavbarStyleThreeComponent, DownloadAppStyleTwoComponent, FooterStyleFourComponent, BackToTopComponent, NavbarStyleTwoComponent],
+    imports: [RouterLink, NavbarStyleTwoComponent, DownloadAppStyleTwoComponent, FooterStyleFourComponent, BackToTopComponent],
     templateUrl: './terms-conditions-page.component.html',
     styleUrls: ['./terms-conditions-page.component.scss']
 })
