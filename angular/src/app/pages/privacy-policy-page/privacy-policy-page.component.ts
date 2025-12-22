1a6b82d6f9f330e92d409d20f4fbb42809c08a1c
minawagdy <minaw77@gmail.com>
2025-12-22 16:00:49 +0200

    update privacy-policy-page.component.ts

--- angular/src/app/pages/privacy-policy-page/privacy-policy-page.component.ts
+++ angular/src/app/pages/privacy-policy-page/privacy-policy-page.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Router, RouterLink } from '@angular/router';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
-import { TranslateService } from '../../../translate.service';
-
+import { RouterLink } from '@angular/router';
 import { NavbarStyleTwoComponent } from '../../common/navbar-style-two/navbar-style-two.component';
 import { DownloadAppStyleTwoComponent } from '../../common/download-app-style-two/download-app-style-two.component';
 import { FooterStyleFourComponent } from '../../common/footer-style-four/footer-style-four.component';
@@ -28,26 +26,13 @@ import { BackToTopComponent } from '../../common/back-to-top/back-to-top.compone
 })
 export class PrivacyPolicyPageComponent implements OnInit {
 
-    currentLang: 'en' | 'ar' = 'en';
     title = '';
     content!: SafeHtml;
 
     constructor(
         private http: HttpClient,
-        private router: Router,
-        private sanitizer: DomSanitizer,
-        public translate: TranslateService
-    ) {
-        // 🔹 Detect language from URL
-        const urlLang = this.router.url.split('/')[1] as 'en' | 'ar';
-        this.currentLang = urlLang === 'ar' ? 'ar' : 'en';
-
-        this.translate.switchLang(this.currentLang);
-
-        // 🔹 Set HTML attributes
-        document.documentElement.lang = this.currentLang;
-        document.documentElement.dir = this.currentLang === 'ar' ? 'rtl' : 'ltr';
-    }
+        private sanitizer: DomSanitizer
+    ) { }
 
     ngOnInit(): void {
         this.getPrivacyPolicy();
@@ -56,26 +41,14 @@ export class PrivacyPolicyPageComponent implements OnInit {
     getPrivacyPolicy(): void {
         this.http
             .get<any>('https://admin.realstatecrm-development.dev.alefsoftware.com/site/privacy-policy')
-            .subscribe({
-                next: (res) => {
-                    if (res?.status && res?.data) {
+            .subscribe(res => {
+                if (res?.status && res?.data) {
+                    this.title = res.data.title_en;
 
-                        // ✅ Localized title
-                        this.title = this.currentLang === 'ar'
-                            ? res.data.title_ar
-                            : res.data.title_en;
-
-                        // ✅ Localized HTML content
-                        const description =
-                            this.currentLang === 'ar'
-                                ? res.data.description_ar
-                                : res.data.description_en;
-
-                        this.content = this.sanitizer.bypassSecurityTrustHtml(description);
-                    }
-                },
-                error: (err) => {
-                    console.error('Failed to load privacy policy:', err);
+                    // VERY IMPORTANT
+                    this.content = this.sanitizer.bypassSecurityTrustHtml(
+                        res.data.description_en
+                    );
                 }
             });
     }
