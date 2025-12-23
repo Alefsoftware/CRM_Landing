8b2f2ab6660527cacc3343ee848da1c70d7e6517
minawagdy <minaw77@gmail.com>
2025-12-23 16:32:57 +0200

    update intro-video.component.html and intro-video.component.ts

--- angular/src/app/common/intro-video/intro-video.component.ts
+++ angular/src/app/common/intro-video/intro-video.component.ts
@@ -4,19 +4,29 @@ import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-intro-video',
+    standalone: true,
     imports: [RouterLink, NgIf],
     templateUrl: './intro-video.component.html',
     styleUrls: ['./intro-video.component.scss']
 })
 export class IntroVideoComponent {
 
-    // Video Popup
     isOpen = false;
+
+    // coming from backend
+    videoLink = 'https://www.youtube.com/watch?v=E8QNqEk9t8o';
+
+    get embedVideoUrl(): string {
+        if (!this.videoLink) return '';
+        const videoId = this.videoLink.split('v=')[1]?.split('&')[0];
+        return `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+    }
+
     openPopup(): void {
         this.isOpen = true;
     }
+
     closePopup(): void {
         this.isOpen = false;
     }
-
 }
