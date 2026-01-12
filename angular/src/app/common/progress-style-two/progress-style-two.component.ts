507f024e2aa07dfcebb601f7e4116df480d88772
minawagdy <minaw77@gmail.com>
2026-01-12 14:09:00 +0200

    update progress-style-two.component.html, progress-style-two.component.ts and about-simple-page.component.html

--- angular/src/app/common/progress-style-two/progress-style-two.component.ts
+++ angular/src/app/common/progress-style-two/progress-style-two.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+// progress-style-two.component.ts
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { LanguageService } from '../../services/language.service'; // Adjust path
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-progress-style-two',
-    imports: [RouterLink],
+    imports: [RouterLink, CommonModule],
     templateUrl: './progress-style-two.component.html',
     styleUrls: ['./progress-style-two.component.scss']
 })
-export class ProgressStyleTwoComponent {}
+export class ProgressStyleTwoComponent implements OnInit {
+    currentLang: 'en' | 'ar' = 'en';
+
+    constructor(private languageService: LanguageService) { }
+
+    ngOnInit(): void {
+        this.currentLang = this.languageService.getCurrentLang();
+
+        // Subscribe to language changes
+        this.languageService.currentLang$.subscribe(lang => {
+            this.currentLang = lang;
+        });
+    }
+}
